b4046d8657547aee07df3afcf1f7f5a47405d8f6
MetalUpYourAss <djapar-shah@mail.ru>
2020-03-17 18:12:23 +0300

    Допеределал основной экран

--- app/src/main/java/com/djaphar/coffeepointsappcourier/Activities/MainActivity.java
+++ app/src/main/java/com/djaphar/coffeepointsappcourier/Activities/MainActivity.java
@@ -1,12 +1,16 @@
 package com.djaphar.coffeepointsappcourier.Activities;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.djaphar.coffeepointsappcourier.R;
 import com.djaphar.coffeepointsappcourier.SupportClasses.ProductsRecyclerViewAdapter;
 import com.djaphar.coffeepointsappcourier.ViewModels.MainViewModel;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
     private MainViewModel mainViewModel;
     private RecyclerView productsRecyclerView;
+    private SwitchCompat statusSwitch;
+    private TextView statusTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +31,36 @@ public class MainActivity extends AppCompatActivity {
         mainViewModel.getProducts().observe(this, products -> {
             ProductsRecyclerViewAdapter adapter = new ProductsRecyclerViewAdapter(products);
             productsRecyclerView.setAdapter(adapter);
+            productsRecyclerView.setNestedScrollingEnabled(false);
             productsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         });
+        statusSwitch = findViewById(R.id.status_switch);
+        statusTv = findViewById(R.id.status_tv);
+        TextView exitTv = findViewById(R.id.exit_tv);
+
+        statusSwitch.setOnCheckedChangeListener((compoundButton, isChecked) -> {
+            if (isChecked) {
+                statusTv.setText(R.string.status_true);
+                statusTv.setTextColor(getResources().getColor(R.color.colorGreen87));
+            } else {
+                statusTv.setText(R.string.status_false);
+                statusTv.setTextColor(getResources().getColor(R.color.colorRed87));
+            }
+        });
+
+        exitTv.setOnClickListener(lView -> createLogoutDialog());
+    }
+
+    private void createLogoutDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder
+            .setTitle(R.string.logout_dialog_title)
+            .setMessage(R.string.logout_dialog_message)
+            .setNegativeButton(R.string.logout_dialog_negative_btn, (dialogInterface, i) -> dialogInterface.cancel())
+            .setPositiveButton(R.string.logout_dialog_positive_btn, (dialogInterface, i) -> {
+                mainViewModel.logout();
+                Toast.makeText(this, R.string.ononoki_chan, Toast.LENGTH_SHORT).show();
+            })
+            .show();
     }
 }
--- app/src/main/java/com/djaphar/coffeepointsappcourier/ViewModels/MainViewModel.java
+++ app/src/main/java/com/djaphar/coffeepointsappcourier/ViewModels/MainViewModel.java
@@ -1,6 +1,7 @@
 package com.djaphar.coffeepointsappcourier.ViewModels;
 
 import android.app.Application;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -24,6 +25,9 @@ public class MainViewModel extends AndroidViewModel {
         String product5 ="Блинчики";
         String product6 ="Мороженка 2";
         String product7 ="Блинчики 2";
+        String product8 ="Блинчики 3";
+        String product9 ="Блинчики 4";
+        String product10 ="Блинчики 5";
 
         productList.add(product1);
         productList.add(product2);
@@ -32,6 +36,9 @@ public class MainViewModel extends AndroidViewModel {
         productList.add(product5);
         productList.add(product6);
         productList.add(product7);
+        productList.add(product8);
+        productList.add(product9);
+        productList.add(product10);
 
         products.setValue(productList);
     }
@@ -39,4 +46,6 @@ public class MainViewModel extends AndroidViewModel {
     public MutableLiveData<ArrayList<String>> getProducts() {
         return products;
     }
+
+    public void logout() { }
 }
